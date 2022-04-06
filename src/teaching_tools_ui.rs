d3ff6d25b3d175b731b4a1b0114b0588182381d8
endre90 <endre90@gmail.com>
2022-04-06 22:55:44 +0200

    scenario_2 and ur_controller

--- src/teaching_tools_ui.rs
+++ src/teaching_tools_ui.rs
@@ -1,8 +1,15 @@
+use futures::Stream;
+use futures::StreamExt;
 use iced::{
     button, executor, window, Alignment, Application, Button, Column, Command, Element, Settings,
     Text,
 };
+use r2r::sensor_msgs::msg::JointState;
+use r2r::std_msgs::msg::Header;
 use r2r::std_srvs::srv::Trigger;
+use r2r::ur_controller_msgs::action::URControl;
+use r2r::ActionClient;
+use r2r::QosProfile;
 use std::sync::{Arc, Mutex};
 
 pub static NODE_ID: &'static str = "teaching_tools_ui";
@@ -13,8 +20,9 @@ struct TeachingToolsUI {
     match_button: button::State,
     reset_ghost_client: Arc<Mutex<r2r::Client<Trigger::Service>>>,
     reset_marker_client: Arc<Mutex<r2r::Client<Trigger::Service>>>,
-    match_ghost_client: Arc<Mutex<r2r::Client<Trigger::Service>>>,
-    // waiting_for_reset_ghost_server: impl Future<Output = r2r::Result<()>>
+    match_ghost_client:
+        Arc<Mutex<ActionClient<r2r::ur_controller_msgs::action::URControl::Action>>>,
+    ghost_state: Arc<Mutex<JointState>>, // waiting_for_reset_ghost_server: impl Future<Output = r2r::Result<()>>
 }
 
 pub fn main() -> iced::Result {
@@ -36,6 +44,8 @@ enum Message {
     Match,
 }
 
+// instead of the match service and so, listen to the ghost joint state here and
+// send that as the move request using 'use_joint_state'
 impl Application for TeachingToolsUI {
     type Executor = executor::Default;
     type Message = Message;
@@ -53,15 +63,38 @@ impl Application for TeachingToolsUI {
             node.create_client::<Trigger::Service>("reset_teaching_marker")
                 .expect("could not create reset marker client"),
         ));
+        // listen to the current teaching ghost pose to go to (in teaching mode?)
+        let ghost_state_subscriber = node
+            .subscribe::<JointState>("ghost/joint_states", QosProfile::default())
+            .expect("could not joint state subscriber");
         let match_ghost_client = Arc::new(Mutex::new(
-            node.create_client::<Trigger::Service>("match_ghost")
-                .expect("could not create match ghost client"),
+            node.create_action_client::<URControl::Action>("ur_control")
+                .expect("could not create ur control client"),
         ));
+
+        // initialize the ghost joint state
+        let ghost_joint_state = Arc::new(Mutex::new(JointState {
+            header: Header {
+                ..Default::default()
+            },
+            ..Default::default()
+        }));
+
+        let ghost_joint_state_clone_1 = ghost_joint_state.clone();
+        tokio::task::spawn(async move {
+            match ghost_subscriber_callback(ghost_state_subscriber, &ghost_joint_state_clone_1)
+                .await
+            {
+                Ok(()) => (),
+                Err(e) => r2r::log_error!(NODE_ID, "Joint state subscriber failed with {}.", e),
+            };
+        });
 
         let _handle = std::thread::spawn(move || loop {
             node.spin_once(std::time::Duration::from_millis(100));
         });
 
+        let ghost_joint_state_clone_2 = ghost_joint_state.clone();
         (
             TeachingToolsUI {
                 reset_button: button::State::new(),
@@ -69,6 +102,7 @@ impl Application for TeachingToolsUI {
                 reset_ghost_client: reset_ghost_client.clone(),
                 reset_marker_client: reset_marker_client.clone(),
                 match_ghost_client: match_ghost_client.clone(),
+                ghost_state: ghost_joint_state_clone_2,
             },
             Command::none(),
         )
@@ -90,11 +124,10 @@ impl Application for TeachingToolsUI {
                     |_| Message::Empty,
                 )
             }
-            Message::Match => {
-                Command::perform(match_ghost(self.match_ghost_client.clone()), |_| {
-                    Message::Empty
-                })
-            }
+            Message::Match => Command::perform(
+                match_ghost(self.match_ghost_client.clone(), self.ghost_state.clone()),
+                |_| Message::Empty,
+            ),
             Message::Empty => Command::none(),
         }
     }
@@ -112,6 +145,26 @@ impl Application for TeachingToolsUI {
                     .on_press(Message::Match),
             )
             .into()
+    }
+}
+
+// subscribe to the current joint pose of the ghost robot
+async fn ghost_subscriber_callback(
+    mut subscriber: impl Stream<Item = JointState> + Unpin,
+    ghost_joint_state: &Arc<Mutex<JointState>>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        match subscriber.next().await {
+            Some(msg) => {
+                let mut new_joint_state = ghost_joint_state.lock().unwrap().clone();
+                new_joint_state.position = msg.position;
+                *ghost_joint_state.lock().unwrap() = new_joint_state;
+            }
+            None => {
+                r2r::log_error!(NODE_ID, "Subscriber did not get the message?");
+                ()
+            }
+        }
     }
 }
 
@@ -166,29 +219,94 @@ async fn reset_ghost_and_marker(
 }
 
 // ask the robot to go where the ghost is
-async fn match_ghost(client: Arc<Mutex<r2r::Client<Trigger::Service>>>) -> Option<()> {
+async fn match_ghost(
+    client: Arc<Mutex<ActionClient<r2r::ur_controller_msgs::action::URControl::Action>>>,
+    joint_state: Arc<Mutex<JointState>>,
+) -> Option<()> {
     let request_msg = Trigger::Request {};
+    let joint_state_local = joint_state.lock().unwrap().clone();
+    let goal = URControl::Goal {
+        command: "move_j".to_string(),
+        use_joint_positions: true,
+        joint_positions: joint_state_local,
+        ..Default::default()
+    };
 
-    let request = client
-        .lock()
-        .unwrap()
-        .request(&request_msg)
-        .expect("Could not send reset ghost request.");
+    let c = client.lock().unwrap().clone();
 
-    r2r::log_info!(NODE_ID, "Request to reset the ghost sent.");
-
-    // are the ghost and the marker even responding or not?
-    r2r::log_info!(NODE_ID, "Request to reset the ghost sent.");
-    let response = request.await.expect("asdf");
-
-    match response.success {
-        true => {
-            r2r::log_info!(NODE_ID, "Ghost and marker are reset.");
-            Some(())
+    let (_goal, result, _feedback) = match c.send_goal_request(goal) {
+        Ok(x) => match x.await {
+            Ok(y) => y,
+            Err(_) => {
+                r2r::log_info!(NODE_ID, "Could not send goal request.");
+                return None;
+            }
+        },
+        Err(_) => {
+            r2r::log_info!(NODE_ID, "Did not get goal.");
+            return None;
         }
-        false => {
-            r2r::log_error!(NODE_ID, "Couldn't reset marker.",);
+    };
+
+    match result.await {
+        Ok((status, msg)) => match status {
+            r2r::GoalStatus::Aborted => {
+                r2r::log_info!(NODE_ID, "Goal succesfully aborted with: {:?}", msg);
+                Some(())
+            }
+            _ => {
+                r2r::log_info!(
+                    NODE_ID,
+                    "Executing the Simple Robot Simulator Command succeeded."
+                );
+                Some(())
+            }
+        },
+        Err(e) => {
+            r2r::log_error!(
+                NODE_ID,
+                "Simple Robot Simulator Action failed with: {:?}",
+                e,
+            );
             None
         }
     }
 }
+
+// async fn execute_event_based_command(
+//     goal: SimpleRobotControl::Goal,
+//     client: &r2r::ActionClient<SimpleRobotControl::Action>,
+// ) -> bool {
+//     r2r::log_info!(NODE_ID, "Sending request to Simple Robot Simulator.");
+
+//     let (_goal, result, _feedback) = match client.send_goal_request(goal) {
+//         Ok(x) => match x.await {
+//             Ok(y) => y,
+//             Err(_) => {
+//                 r2r::log_info!(NODE_ID, "Could not send goal request.");
+//                 return false;
+//             }
+//         },
+//         Err(_) => {
+//             r2r::log_info!(NODE_ID, "Did not get goal.");
+//             return false;
+//         }
+//     };
+
+//     match result.await {
+//         Ok((status, msg)) => match status {
+//             r2r::GoalStatus::Aborted => {
+//                 r2r::log_info!(NODE_ID, "Goal succesfully aborted with: {:?}", msg);
+//                 true
+//             }
+//             _ => {
+//                 r2r::log_info!(NODE_ID, "Executing the Simple Robot Simulator Command succeeded.");
+//                 true
+//             }
+//         },
+//         Err(e) => {
+//             r2r::log_error!(NODE_ID, "Simple Robot Simulator Action failed with: {:?}", e,);
+//             false
+//         }
+//     }
+// }
